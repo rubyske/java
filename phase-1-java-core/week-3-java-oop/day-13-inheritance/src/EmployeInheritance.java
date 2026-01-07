ec2f82526f071755fe23567c48026613ac58012a
Muhamad Badru Kamal <badrukamal327@gmail.com>
2026-01-07 11:06:58 +0700

    LJ-006 : Materi Phase 1 week 3 ( OOP Inherintance )

---
+++ phase-1-java-core/week-3-java-oop/day-13-inheritance/src/EmployeInheritance.java
@@ -0,0 +1,10 @@
+package src;
+
+public class EmployeInheritance {
+    protected String name;
+    protected double salary;
+
+    public void work(){
+        System.out.println("employees are working");
+    }
+}
